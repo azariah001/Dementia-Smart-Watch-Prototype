a40c72ee491a97a60f033164f00c50f26d19c4ea
Viktor Polak <viktor.polak@connect.qut.edu.au>
2014-08-12 20:58:46 +1000

    Initial Commit

---
+++ SmartwatchAdministrator/gen/com/illusivemen/smartwatchadministrator/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.illusivemen.smartwatchadministrator;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
