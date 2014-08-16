3513a04cdc9d03c2a3d76ccbb99e461275e60489
Viktor Polak <viktor.polak@connect.qut.edu.au>
2014-08-16 18:24:13 +1000

    Client map working, updating and simple.

--- SmartwatchClient/src/com/illusivemen/smartwatchclient/GoogleMapping.java
+++ SmartwatchClient/src/com/illusivemen/smartwatchclient/GoogleMapping.java
@@ -3,40 +3,49 @@ package com.illusivemen.smartwatchclient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
 public class GoogleMapping extends Activity {
 	
 	private GoogleMap googleMap;
+	private LocationManager locationManager;
+	private Marker myMarker;
+	private static final long MIN_UPDATE_MILLISEC = 250;
+	private static final float MIN_UPDATE_METRES = 1;
+	private static final float INITIAL_ZOOM= 18;
+	private String purpose;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_google_mapping);
 		
-		try {
-            // Loading map
-            initilizeMap();
- 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-		
-		// What is the purpose of this map?
+		// What is the Function of this Map?
 		Intent source = getIntent();
-		String job = source.getStringExtra(MainMenu.ACTIVITY_MESSAGE);
-		switch (job) {
+		purpose = source.getStringExtra(MainMenu.ACTIVITY_MESSAGE);
+		
+		// Title by Function
+		switch (purpose) {
 		case "LIVE_MAP":
 			this.setTitle("Current Location");
+			locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 			break;
 		}
+		
+		// Create Map
+        initilizeMap();
 	}
 	
 	/**
@@ -46,15 +55,18 @@ public class GoogleMapping extends Activity {
         if (googleMap == null) {
             googleMap = ((MapFragment) getFragmentManager().findFragmentById(
                     R.id.map)).getMap();
-            // show location position
-            googleMap.setMyLocationEnabled(true);
-            // move to location and zoom in
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-27, 153), 6));
+            // set type of map to use
+            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            // disable unnecessary gestures
+            disableAdvancedUi();
+            
+            // location updates
+            subscribeForLocation();
  
-            // check if map is created successfully or not
+            // make sure map is created
             if (googleMap == null) {
                 Toast.makeText(getApplicationContext(),
-                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        "Sorry! Unable to open map.", Toast.LENGTH_SHORT)
                         .show();
             }
         }
@@ -65,23 +77,63 @@ public class GoogleMapping extends Activity {
         super.onResume();
         initilizeMap();
     }
+    
+    private void subscribeForLocation() {
+    	// use any location source
+    	Criteria criteria = new Criteria();
+		String provider = locationManager.getBestProvider(criteria, false);
+		// start by using last known location
+		Location location = locationManager.getLastKnownLocation(provider);
+		LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+		
+		// position map and current location marker
+		googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, INITIAL_ZOOM));
+		// create custom icon for current position
+		myMarker = googleMap.addMarker(new MarkerOptions()
+		        .position(latLng)
+		        .title("Current Location")
+		        .icon(BitmapDescriptorFactory.fromResource(R.drawable.person)));
+		
+		// listen for location updates
+		locationManager.requestLocationUpdates(
+					provider, MIN_UPDATE_MILLISEC, MIN_UPDATE_METRES, locationListener);
+		
+    }
+    
+    /**
+     * Disable functions that manipulate the map other than the zoom levels.
+     */
+    private void disableAdvancedUi() {
+    	googleMap.getUiSettings().setZoomGesturesEnabled(false);
+        googleMap.getUiSettings().setRotateGesturesEnabled(false);
+        googleMap.getUiSettings().setTiltGesturesEnabled(false);
+        googleMap.getUiSettings().setScrollGesturesEnabled(false);
+        googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+    }
+	
+	private final LocationListener locationListener = new LocationListener() {
+	    @Override
+	    public void onLocationChanged(final Location location) {
+	    	// move to location
+	        LatLng newPosition = new LatLng(location.getLatitude(), location.getLongitude());
+	        googleMap.animateCamera(CameraUpdateFactory.newLatLng(newPosition));
+	        myMarker.setPosition(newPosition);
+	    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.google_mapping, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		@Override
+		public void onProviderDisabled(String provider) {
+			
 		}
-		return super.onOptionsItemSelected(item);
-	}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+			
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			
+			
+		}
+	};
 }
