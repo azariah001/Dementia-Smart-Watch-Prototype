5588a4391a93bf2016631cbd51b081e536484a2e
richa_000 <richa_000@131.181.26.61>
2014-09-02 03:47:52 +1000

    Security Settings partly implemented. IM-46

---
+++ SmartwatchClient/src/com/illusivemen/smartwatchclient/MainSmartwatchSettingActivity.java
@@ -0,0 +1,59 @@
+package com.illusivemen.smartwatchclient;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainSmartwatchSettingActivity extends Activity {
+	
+	private static final int RESULT_SETTINGS = 1;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_menu);
+		
+		showUserSettings();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.settings, menu);
+		return true;
+	}
+	
+
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode,  resultCode, data);
+		
+		switch (requestCode) {
+		case RESULT_SETTINGS:
+			showUserSettings();
+			break;
+		}
+	}
+	
+	private void showUserSettings() {
+		SharedPreferences sharedPrefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		
+		StringBuilder builder = new StringBuilder();
+		
+		builder.append("\n Username: "
+				+ sharedPrefs.getString("prefUsername",  "NULL"));
+		
+		builder.append("\n Send report:"
+				+ sharedPrefs.getBoolean("prefSendReport", false));
+		
+		builder.append("\n Sync Frequency: "
+				+ sharedPrefs.getString("prefSyncFrequency", "NULL"));
+	}
+	
+}
---
+++ SmartwatchClient/src/com/illusivemen/smartwatchclient/SmartwatchSettingActivity.java
@@ -0,0 +1,15 @@
+package com.illusivemen.smartwatchclient;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SmartwatchSettingActivity extends PreferenceActivity{
+	
+	@SuppressWarnings("deprecation")
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		addPreferencesFromResource(R.xml.settings);
+	}
+	
+}
