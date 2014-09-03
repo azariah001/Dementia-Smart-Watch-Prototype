f216c06a87b50f3aa841050480dd7cab9b1072ec
Daniel Luckhurst <daniel.luckhurst@connect.qut.edu.au>
2014-09-04 09:48:22 +1000

    IM-13 Low Battery Alert

--- SmartwatchClient/src/com/illusivemen/smartwatchclient/MainMenu.java
+++ SmartwatchClient/src/com/illusivemen/smartwatchclient/MainMenu.java
@@ -1,27 +1,40 @@
 package com.illusivemen.smartwatchclient;
 
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import com.illusivemen.mapping.GoogleMapping;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.location.LocationManager;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainMenu extends Activity {
 
 	public final static String ACTIVITY_MESSAGE = "ClientStart";
-	
+	private static final long CHECK_BATTERY_STATE = 250;
 	
 	//For SOS Beacon.//0 = not panicked.//1 = very panicked
 	private int panic = 0;
+	private boolean lowBatteryAlert = false;
+	private float batteryPct;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_menu);
+		new Timer().scheduleAtFixedRate(lowBattery, 0, CHECK_BATTERY_STATE);
 	}
 
 	@Override
@@ -49,6 +62,20 @@ public class MainMenu extends Activity {
 	
 	public void showProfile(View view) {
 		startActivity(PatientProfile.makeIntent(MainMenu.this, ACTIVITY_MESSAGE));
+	}
+	
+	// for testing purposes
+	public void showBattery(View view) {
+		Context context = getApplicationContext();
+		int duration = Toast.LENGTH_LONG;
+		String alertText;
+		if (lowBatteryAlert == true) {
+			alertText = "Low Battery Alert";
+		} else {
+			alertText = "Battery is " + batteryPct + "% charged";
+		}
+		Toast toast = Toast.makeText(context, alertText, duration);
+		toast.show();
 	}
 	
 	//Panic notification dialogbox, creates on click.
@@ -92,5 +119,24 @@ public class MainMenu extends Activity {
 		
 		//TODO update server db with current panic state;
 		
-	}	
+	}
+	
+	TimerTask lowBattery = new TimerTask() {
+		
+		public void run() {
+		
+			IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+			Intent battery = registerReceiver(null, ifilter);
+		
+			int level = battery.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+			int scale = battery.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+			float percent = level / (float)scale;
+			batteryPct = percent; 
+			
+			if (percent < 0.10) {
+				lowBatteryAlert = true;
+			}			
+		}
+	};
 }
